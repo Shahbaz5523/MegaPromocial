3e1bcad8549cf41a00c14f12a0dda3673e9ce3de
Shahbaz5523 <shahbaz.rajpoot15@gmail.com>
2021-08-11 19:08:18 +0500

    contact us and favicon add

--- src/js/app.js
+++ src/js/app.js
@@ -2,3 +2,9 @@ const toggleMenu = () => {
     const nav = document.getElementById('navbar');
     nav.classList.toggle('mobile');
 }
+
+const key = setInterval(() => {
+    if(document.getElementsByClassName('eapps-link').length > 0){
+        document.getElementsByClassName('eapps-link')[0].style="dispaly:none";
+    }
+}, 500);
